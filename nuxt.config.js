df426a6451d992d16d6323b6a0a602308d642e0a
masato <masa.5tonic@gmail.com>
2020-07-02 21:03:48 +0900

    deleted favicon and changed the title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,13 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Abroach',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap' }
     ]
   },
